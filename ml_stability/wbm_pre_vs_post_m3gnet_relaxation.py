1c3cc92bf1f7c36c157bb1e1d9d71a0ef2cf2c63
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-08-15 09:56:59 -0700

    add ml_stability/m3gnet/plots/2022-08-02-m3gnet-wbm-hull-dist-hist.pdf
    and plots/from-rhys/2022-08-06-hist-wbm-batches-true-energy-all-elements.pdf

--- ml_stability/wbm_pre_vs_post_m3gnet_relaxation.py
+++ ml_stability/wbm_pre_vs_post_m3gnet_relaxation.py
@@ -24,34 +24,34 @@ df_wbm = pd.read_json(
 ).set_index("material_id")
 
 
-# %% spread M3GNet post-pseudo-relaxation lattice params into separate columns
+# %%
 df_m3gnet = pd.read_json(
-    f"{ROOT}/data/2022-07-17@20-27-m3gnet-wbm-relax-results.json.gz"
+    f"{ROOT}/data/2022-07-17-m3gnet-wbm-relax-results.json.gz"
 ).set_index("material_id")
 
 print("Number of WBM crystals for which we have M3GNet results:")
 print(f"{len(df_m3gnet):,} / {len(df_wbm):,} = {len(df_m3gnet)/len(df_wbm):.1%}")
 
 
-# %%
+# %% spread M3GNet post-pseudo-relaxation lattice params into separate columns
 df_m3gnet["final_energy"] = df_m3gnet.trajectory.map(lambda x: x["energies"][-1][0])
 
 df_m3gnet_lattice = pd.json_normalize(
-    df_m3gnet.final_structure.map(lambda x: x["lattice"])
+    df_m3gnet.initial_structure.map(lambda x: x["lattice"])
 ).add_prefix("m3gnet_")
 df_m3gnet[df_m3gnet_lattice.columns] = df_m3gnet_lattice.to_numpy()
 df_m3gnet
 
 
 # %% spread WBM initial and final lattice params into separate columns
-df_m3gnet["cse"] = df_wbm.cse
+df_m3gnet["final_wbm_structure"] = df_wbm.cse.map(lambda x: x["structure"])
 df_wbm_final_lattice = pd.json_normalize(
-    df_m3gnet.cse.map(lambda x: x["structure"]["lattice"])
+    df_m3gnet.final_wbm_structure.map(lambda x: x["lattice"])
 ).add_prefix("final_wbm_")
 df_m3gnet[df_wbm_final_lattice.columns] = df_wbm_final_lattice.to_numpy()
 
 
-df_m3gnet["initial_structure"] = df_wbm.initial_structure
+df_m3gnet["initial_wbm_structure"] = df_wbm.initial_structure
 df_wbm_initial_lattice = pd.json_normalize(
     df_m3gnet.initial_structure.map(lambda x: x["lattice"])
 ).add_prefix("initial_wbm_")
@@ -67,7 +67,7 @@ df_wbm = df_wbm.join(df_wbm_final_lattice)
 df_wbm_initial_lattice = pd.json_normalize(
     df_wbm.initial_structure.map(lambda x: x["lattice"])
 ).add_prefix("initial_wbm_")
-df_wbm[df_wbm_initial_lattice.columns] = df_wbm_initial_lattice
+df_wbm[df_wbm_initial_lattice.columns] = df_wbm_initial_lattice.to_numpy()
 
 print(f"{df_wbm.isna().sum()=}")
 
@@ -102,7 +102,6 @@ fig.show()
 fig = px.histogram(
     df_m3gnet.filter(like="alpha"), nbins=1000, barmode="overlay", log_y=True
 )
-# fig.write_image("plots/alpha-wbm-angles.png", scale=2)
 fig.show()
 
 
