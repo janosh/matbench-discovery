1e9645840f2241d5744a2f5290085f6344c37f5f
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-08-16 05:18:47 +0100

    add ml_stability/m3gnet/slurm_array_m3gnet_relax_wbm.py
    mv ml_stability/m3gnet/{m3gnet_relax_wbm->join_and_plot_m3gnet_relax_results}.py

---
+++ ml_stability/m3gnet/join_and_plot_m3gnet_relax_results.py
@@ -0,0 +1,105 @@
+# %%
+from __future__ import annotations
+
+import gzip
+import io
+import pickle
+from datetime import datetime
+from glob import glob
+from urllib.request import urlopen
+
+import pandas as pd
+from diel_frontier.patched_phase_diagram import load_ppd
+from pymatgen.analysis.phase_diagram import PDEntry
+from pymatgen.core import Structure
+from tqdm import tqdm
+
+from ml_stability import ROOT
+from ml_stability.plots.plot_funcs import hist_classify_stable_as_func_of_hull_dist
+
+
+today = f"{datetime.now():%Y-%m-%d}"
+
+
+# %%
+glob_pattern = "2022-08-16-m3gnet-wbm-relax-results/*.json.gz"
+file_paths = glob(f"{ROOT}/data/{glob_pattern}")
+print(f"Found {len(file_paths):,} files for {glob_pattern = }")
+
+
+dfs: dict[str, pd.DataFrame] = {}
+# 2022-08-16 tried multiprocessing.Pool() to load files in parallel but was somehow
+# slower than serial loading
+for file_path in tqdm(file_paths):
+    if file_path in dfs:
+        continue
+    try:
+        dfs[file_path] = pd.read_json(file_path)
+    except (ValueError, FileNotFoundError):
+        # pandas v1.5+ correctly raises FileNotFoundError, below raises ValueError
+        continue
+
+
+# %%
+df_m3gnet = pd.concat(dfs.values())
+df_m3gnet.index.name = "material_id"
+if any(df_m3gnet.index.str.contains("_")):
+    df_m3gnet.index = df_m3gnet.index.str.replace("_", "-")
+
+df_m3gnet = df_m3gnet.rename(
+    columns=dict(final_structure="m3gnet_structure", trajectory="m3gnet_trajectory")
+)
+
+df_m3gnet["m3gnet_energy"] = df_m3gnet.trajectory.map(lambda x: x["energies"][-1][0])
+
+
+# %%
+out_file = f"{today}-m3gnet-wbm-relax-results.json.gz"
+df_m3gnet.reset_index().to_json(f"{ROOT}/data/{out_file}")
+
+
+# %%
+# 2022-01-25-ppd-mp+wbm.pkl.gz (235 MB)
+ppd_pickle_url = "https://figshare.com/ndownloader/files/36669624"
+zipped_file = urlopen(ppd_pickle_url)
+ppd_mp_wbm = pickle.load(io.BytesIO(gzip.decompress(zipped_file.read())))
+
+ppd_mp_wbm = load_ppd("ppd-mp+wbm-2022-01-25.pkl.gz")
+
+
+df_m3gnet["m3gnet_structure"] = df_m3gnet.m3gnet_structure.map(Structure.from_dict)
+df_m3gnet["pd_entry"] = [
+    PDEntry(row.m3gnet_structure.composition, row.m3gnet_energy)
+    for row in df_m3gnet.itertuples()
+]
+df_m3gnet["e_form_m3gnet"] = df_m3gnet.pd_entry.map(ppd_mp_wbm.get_form_energy_per_atom)
+
+
+# %%
+df_hull = pd.read_csv(
+    f"{ROOT}/data/2022-06-11-from-rhys/wbm-e-above-mp-hull.csv"
+).set_index("material_id")
+
+df_m3gnet["e_above_mp_hull"] = df_hull.e_above_mp_hull
+
+
+df_summary = pd.read_csv(f"{ROOT}/data/wbm-steps-summary.csv", comment="#").set_index(
+    "material_id"
+)
+
+df_m3gnet["e_form_wbm"] = df_summary.e_form
+
+
+# %%
+df_m3gnet.hist(bins=200, figsize=(18, 12))
+df_m3gnet.isna().sum()
+
+
+# %%
+ax_hull_dist_hist = hist_classify_stable_as_func_of_hull_dist(
+    formation_energy_targets=df_m3gnet.e_form_wbm,
+    formation_energy_preds=df_m3gnet.e_form_m3gnet,
+    e_above_hull_vals=df_m3gnet.e_above_mp_hull,
+)
+
+ax_hull_dist_hist.figure.savefig(f"{ROOT}/data/{today}-m3gnet-wbm-hull-dist-hist.pdf")
