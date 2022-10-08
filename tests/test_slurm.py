da2337a80f41eb0d4baf41943c1bab8b53ad892d
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-10-07 19:54:51 -0700

    improve test_slurm_submit(), add tests/test_init.py

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -2,24 +2,42 @@ import sys
 from unittest.mock import patch
 
 import pytest
+from pytest import CaptureFixture
 
 from mb_discovery.slurm import _get_calling_file_path, slurm_submit_python
 
 
-def test_slurm_submit() -> None:
+def test_slurm_submit(capsys: CaptureFixture[str]) -> None:
+    # check slurm_submit_python() does nothing in normal mode
 
+    kwargs = dict(
+        job_name="test_job",
+        log_dir="test_log_dir",
+        time="0:0:1",
+        slurm_flags=("--test-flag",),
+    )
+    slurm_submit_python(**kwargs)  # type: ignore
+
+    stdout, stderr = capsys.readouterr()
+    assert stderr == stderr == ""
+
+    # check slurm_submit_python() does prints cmd calls subprocess.run() in submit mode
     sys.argv += ["slurm-submit"]
-    with pytest.raises(SystemExit) as exc_info, patch(
+
+    with pytest.raises(SystemExit), patch(
         "mb_discovery.slurm.subprocess.run"
-    ) as mock_run:
-        slurm_submit_python(
-            job_name="test_job",
-            log_dir="test_log_dir",
-            time="0:0:1",
-            slurm_flags=("--test_flag",),
-        )
-        assert exc_info.value.code == 0
-        assert mock_run.call_count == 1
+    ) as mock_subprocess_run:
+        slurm_submit_python(**kwargs)  # type: ignore
+
+    assert mock_subprocess_run.call_count == 1
+
+    sbatch_cmd = (
+        "sbatch --partition=icelake --account=LEE-SL3-CPU --time=0:0:1 --job-name "
+        "test_job --output test_log_dir/slurm-%A-%a.out --test-flag --wrap python"
+    )
+    stdout, stderr = capsys.readouterr()
+    assert sbatch_cmd in stdout
+    assert stderr == ""
 
 
 def test_get_calling_file_path() -> None:
