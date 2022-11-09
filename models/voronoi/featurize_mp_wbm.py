784ee96892c51149527d26339360418907d5f30e
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-11-09 21:17:55 +0000

    add slurm_submit_python() to featurize_mp_wbm.py
    print value of important slurm env vars in slurm_submit_python()

--- models/voronoi/featurize_mp_wbm.py
+++ models/voronoi/featurize_mp_wbm.py
@@ -1,5 +1,6 @@
 # %%
 import os
+import warnings
 from datetime import datetime
 
 import pandas as pd
@@ -21,9 +22,25 @@ from pymatgen.core import Structure
 from tqdm import tqdm
 
 from matbench_discovery import ROOT
+from matbench_discovery.slurm import slurm_submit_python
 
 today = f"{datetime.now():%Y-%m-%d}"
 module_dir = os.path.dirname(__file__)
+
+
+# data_path = f"{ROOT}/data/mp/2022-09-16-mp-computed-structure-entries.json.gz"
+data_path = f"{ROOT}/data/wbm/2022-10-19-wbm-init-structs.json.bz2"
+input_col = "structure"
+data_name = "wbm" if "wbm" in data_path else "mp"
+
+slurm_submit_python(
+    job_name=f"voronoi-featurize-{data_name}",
+    partition="icelake-himem",
+    account="LEE-SL3-CPU",
+    time="3:0:0",
+    log_dir=module_dir,
+    slurm_flags=("--mem=40G",),
+)
 
 
 # %% Create the featurizer: Ward et al. use a variety of different featurizers
@@ -44,16 +61,28 @@ featurizer = MultipleFeaturizer(
 
 
 # %%
-data_path = f"{ROOT}/data/mp/2022-09-16-mp-computed-structure-entries.json.gz"
-# data_path = f"{ROOT}/data/wbm/2022-10-19-wbm-cses+init-structs.json.bz2"
 df = pd.read_json(data_path).set_index("material_id")
 
-df["structure"] = [Structure.from_dict(x["structure"]) for x in tqdm(df.entry)]
+if data_name == "mp":
+    struct_dicts = [x["structure"] for x in df.entry]
+if data_name == "wbm":
+    struct_dicts = df.initial_structure
+
+df[input_col] = [
+    Structure.from_dict(x) for x in tqdm(df.initial_structure, disable=None)
+]
+
+
+# %% prints lots of pymatgen warnings
+# > No electronegativity for Ne. Setting to NaN. This has no physical meaning, ...
+warnings.filterwarnings(action="ignore", category=UserWarning, module="pymatgen")
+
+df_features = featurizer.featurize_dataframe(
+    df, input_col, ignore_errors=True, pbar=True
+)
 
 
 # %%
-df_featurized = featurizer.featurize_dataframe(df, "structure", ignore_errors=True)
-
-
-# %%
-df_featurized.to_json(f"{module_dir}/{today}mp-train-voronoi-tesselation.json.gz")
+df_features.to_json(
+    f"{module_dir}/{today}-voronoi-tesselation-{data_name}-features.json.gz"
+)
