9f0468162a152153461522674c789247a4f2b533
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-12-02 09:43:36 -0800

    embed pdf figures on landing page
    requires removing single quotes from file names to avoid build errors
    
    rendering chunks (1)...[vite:esbuild-transpile] Transform failed with 1 error:
    _app/immutable/components/pages/_page.svelte-c6406821.js:5:97: ERROR: Expected ")" but found "true"
    
    Expected ")" but found "true"
    3  |  const __vite_glob_0_0 = ""+new URL('../../assets/2022-08-16-m3gnet-wbm-hull-dist-hist-99f39fe9.pdf', import.meta.url).href+"";
    4  |
    5  |  const __vite_glob_0_1 = ""+new URL('../../assets/2022-08-25-wren-wbm-hull-dist-hist-energy_type_'true'-criterion_'energy'-7a843898.pdf', import.meta.url).href+"";
       |                                                                                                   ^
    6  |
    7  |  const __vite_glob_0_2 = ""+new URL('../../assets/2022-09-15-precision-recall-vs-calc-count-1c45e983.pdf', import.meta.url).href+""

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
   import Readme from '$root/readme.md'
+
+  const figs = import.meta.glob('$root/figures/*.{png,svg,pdf}', {
+    eager: true,
+    as: 'url',
+  })
 </script>
 
 <Readme />
 
 <h2>Figures</h2>
 
-{#each Object.entries(import.meta.glob( `$root/figures/*.{png,svg}`, { eager: true, as: 'url' } )) as [alt, src]}
-  <img {src} {alt} />
+{#each Object.entries(figs) as [alt, src]}
+  {@const filename = alt.split('/').at(-1)?.split('.')[0]}
+  <h3>{filename?.replaceAll('-', ' ')}</h3>
+  {#if src.endsWith('.pdf')}
+    <embed {src} {alt} width="100%" height="500px" />
+  {:else}
+    <img {src} {alt} />
+  {/if}
 {/each}
 
 <style>
