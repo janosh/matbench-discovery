7852da26a7dce5daacbf8007702d256449bbb338
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-12-27 18:31:01 -0800

    merge all /api/* routes into single page /api
    start adding comments to module globals

--- site/src/routes/api/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit'
-import type { LayoutServerLoad } from './$types'
-
-export const load: LayoutServerLoad = ({ url }) => {
-  if (url.pathname === `/api`) {
-    throw redirect(307, `/api/data`)
-  }
-}
