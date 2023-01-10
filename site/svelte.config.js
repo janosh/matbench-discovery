18be9dcb95ca3f79e143f6894717a78c382ea501
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-10 15:41:45 -0800

    pnpm add -D katex remark-math for equation support in docs

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -1,14 +1,31 @@
 import adapter from '@sveltejs/adapter-static'
+import { fromString } from 'hast-util-from-string'
+import { selectAll } from 'hast-util-select'
+import { toString } from 'hast-util-to-string'
 import { s } from 'hastscript'
+import katex from 'katex'
 import { mdsvex } from 'mdsvex'
-import linkHeadings from 'rehype-autolink-headings'
-import headingSlugs from 'rehype-slug'
+import link_headings from 'rehype-autolink-headings'
+import heading_slugs from 'rehype-slug'
+import math from 'remark-math'
 import preprocess from 'svelte-preprocess'
 
 const rehypePlugins = [
-  headingSlugs,
+  // from https://github.com/kwshi/rehype-katex-svelte
+  (options = {}) =>
+    (tree) => {
+      for (const node of selectAll(`.math-inline,.math-display`, tree)) {
+        const displayMode = node.properties?.className?.includes(`math-display`)
+        const rendered = katex.renderToString(toString(node), {
+          ...options,
+          displayMode,
+        })
+        fromString(node, `{@html ${JSON.stringify(rendered)}}`)
+      }
+    },
+  heading_slugs,
   [
-    linkHeadings,
+    link_headings,
     {
       behavior: `append`,
       test: [`h2`, `h3`, `h4`, `h5`, `h6`], // don't auto-link <h1>
@@ -30,6 +47,9 @@ export default {
     preprocess(),
     mdsvex({
       rehypePlugins,
+      // remark-math@3.0.0 pinned due to mdsvex, see
+      // https://github.com/kwshi/rehype-katex-svelte#usage
+      remarkPlugins: [math],
       extensions: [`.svx`, `.md`],
     }),
   ],
