deec9cbfe6e99ac8c5fe6093c12040fcba70f70f
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-18 10:40:45 -0800

    refactor rolling_mae_vs_hull_dist() to support plotting multiple lines at once
    corresponding to models or WBM batches
    update tests and plotting scripts

--- scripts/rolling_mae_vs_hull_dist.py
+++ scripts/rolling_mae_vs_hull_dist.py
@@ -8,28 +8,30 @@ __date__ = "2022-06-18"
 
 
 # %%
-df_wbm = load_df_wbm_with_preds(models=["Wrenformer"]).round(3)
+model = "Wrenformer"
+df_wbm = load_df_wbm_with_preds([model]).round(3)
 
 e_above_hull_col = "e_above_hull_mp2020_corrected_ppd_mp"
 e_form_col = "e_form_per_atom_mp2020_corrected"
 
 
 # %%
-model_name = "Wrenformer"
-ax = rolling_mae_vs_hull_dist(
+ax, df_err, df_std = rolling_mae_vs_hull_dist(
     e_above_hull_true=df_wbm[e_above_hull_col],
-    e_above_hull_error=df_wbm[e_form_col] - df_wbm[model_name],
-    label=model_name,
+    e_above_hull_errors={model: df_wbm[e_form_col] - df_wbm[model]},
+    # label=model,
     backend=(backend := "plotly"),
-    # template="plotly_white+global",
+    # template="plotly_white",
 )
 
-title = f"{today} {model_name}"
+title = f"{today} {model}"
 if backend == "matplotlib":
     fig = ax.figure
-    fig.set_size_inches(10, 9)
+    fig.set_size_inches(6, 5)
     ax.legend(loc="lower right", frameon=False)
     ax.set(title=title)
+    for line in ax.lines:
+        line._linewidth *= 2
 elif backend == "plotly":
     ax.update_layout(title=dict(text=title, x=0.5))
     ax.show()
--- scripts/rolling_mae_vs_hull_dist_all_models.py
+++ scripts/rolling_mae_vs_hull_dist_all_models.py
@@ -1,5 +1,4 @@
 # %%
-from plotly.subplots import make_subplots
 from pymatviz.utils import save_fig
 
 from matbench_discovery import FIGS, today
@@ -14,45 +13,38 @@ __date__ = "2022-06-18"
 models = sorted(
     "Wrenformer, CGCNN, Voronoi RF, MEGNet, M3GNet, BOWSR MEGNet".split(", ")
 )
+e_form_col = "e_form_per_atom_mp2020_corrected"
+e_above_hull_col = "e_above_hull_mp2020_corrected_ppd_mp"
 
 df_wbm = load_df_wbm_with_preds(models=models).round(3)
 
 
 # %%
-target_col = "e_form_per_atom_mp2020_corrected"
-e_above_hull_col = "e_above_hull_mp2020_corrected_ppd_mp"
 backend: Backend = "plotly"
 
-rows, cols = len(models) // 3, 3
-if backend == "plotly":
-    fig = make_subplots(rows=rows, cols=cols)
+for model in models:
+    model_error = df_wbm[e_form_col] - df_wbm[model]
+    MAE = (df_wbm[e_above_hull_col] - model_error).abs().mean()
+    df_wbm[f"{model} {MAE=:.2f}"] = df_wbm[e_form_col] - df_wbm[model]
+
+fig, df_err, df_std = rolling_mae_vs_hull_dist(
+    e_above_hull_true=df_wbm[e_above_hull_col],
+    e_above_hull_errors=df_wbm.filter(like=" MAE="),
+    backend=backend,
+    # template="plotly_white",
+)
 
 
-for idx, model_name in enumerate(models):
-    row, col = idx % rows + 1, idx // rows + 1
-
-    # assert df_wbm[model_name].isna().sum() < 100
-    preds = df_wbm[target_col] - df_wbm[model_name]
-    MAE = (df_wbm[e_above_hull_col] - preds).abs().mean()
-
-    ax = rolling_mae_vs_hull_dist(
-        e_above_hull_true=df_wbm[e_above_hull_col],
-        e_above_hull_error=preds,
-        label=f"{model_name} · {MAE=:.2f}",
-        backend=backend,
-    )
-    if backend == "plotly":
-        fig.add_traces(ax.data, row=row, col=col)
-
-if hasattr(ax, "legend"):
+if backend == "matplotlib":
     # increase line width in legend
-    legend = ax.legend(frameon=False, loc="lower right")
-    ax.figure.set_size_inches(10, 9)
-    for line in legend.get_lines():
+    legend = fig.legend(frameon=False, loc="lower right")
+    fig.figure.set_size_inches(10, 9)
+    for handle in legend.get_lines():
+        handle._linewidth *= 6
+    for line in fig.lines:
         line._linewidth *= 3
-
-
-fig.show()
+else:
+    fig.show()
 
 
 # %%
