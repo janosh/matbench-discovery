8079ca1645de3a372b5ea2fa18208cd857178533
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-24 11:16:20 -0800

    upgrade /models page with sorting and reporting model metrics (MAE, R2, Recall, ...) and stats (run time, date added, ...)

--- site/src/app.d.ts
+++ site/src/app.d.ts
@@ -3,3 +3,8 @@
 
 declare module '*.md'
 declare module '*package.json'
+
+declare module '*metadata.yml' {
+  const content: import('$lib/types').ModelMetadata
+  export default content
+}
--- site/src/routes/models/+page.server.ts
+++ site/src/routes/models/+page.server.ts
@@ -1,16 +1,16 @@
-import type { ModelMetadata } from '$lib/types'
+import type { ModelData, ModelMetadata } from '$lib/types'
 import { dirname } from 'path'
 import type { PageServerLoad } from './$types'
-import analysis from './2023-01-23-pred-analysis.json'
+import model_stats from './2023-01-23-model-stats.json'
 
 export const load: PageServerLoad = async () => {
   const yml = import.meta.glob(`$root/models/**/metadata.yml`, {
     eager: true,
   })
-  const models: [string, ModelMetadata][] = Object.entries(yml).map(
+  const models: [string, ModelData][] = Object.entries(yml).map(
     ([key, module]) => {
       const metadata = module.default as ModelMetadata
-      const computed = analysis[metadata.model_name] ?? {}
+      const computed = model_stats[metadata.model_name] ?? {}
       return [dirname(key), { ...metadata, ...computed }]
     }
   )
