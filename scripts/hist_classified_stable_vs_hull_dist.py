f0363b4d2bd2e56cfc2db5aa48632ec1b3372611
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-29 19:14:09 -0800

    add stable_metrics() to matbench_discovery/energy.py (formerly part of hist_classified_stable_vs_hull_dist() which gets a big refactor)
    add test_stable_metrics()
    fix wrong sign of model preds in each_pred_col calculation of several plotting scripts

--- scripts/hist_classified_stable_vs_hull_dist.py
+++ scripts/hist_classified_stable_vs_hull_dist.py
@@ -3,6 +3,7 @@ from pymatviz.utils import save_fig
 
 from matbench_discovery import FIGS, today
 from matbench_discovery.data import load_df_wbm_preds
+from matbench_discovery.energy import stable_metrics
 from matbench_discovery.plots import WhichEnergy, hist_classified_stable_vs_hull_dist
 
 __author__ = "Rhys Goodall, Janosh Riebesell"
@@ -24,7 +25,8 @@ df_wbm = load_df_wbm_preds(models=[model_name]).round(3)
 
 # %%
 e_form_col = "e_form_per_atom_mp2020_corrected"
-e_above_hull_col = "e_above_hull_mp2020_corrected_ppd_mp"
+each_true_col = "e_above_hull_mp2020_corrected_ppd_mp"
+each_pred_col = "e_above_hull_pred"
 which_energy: WhichEnergy = "true"
 # std_factor=0,+/-1,+/-2,... changes the criterion for material stability to
 # energy+std_factor*std. energy+std means predicted energy plus the model's uncertainty
@@ -40,25 +42,28 @@ var_aleatoric = (df_wbm.filter(like="_ale_") ** 2).mean(axis=1)
 var_epistemic = df_wbm.filter(regex=r"_pred_\d").var(axis=1, ddof=0)
 std_total = (var_epistemic + var_aleatoric) ** 0.5
 std_total = df_wbm[f"{model_name}_std"]
-e_above_hull_pred = (
-    df_wbm[e_above_hull_col]
-    + (df_wbm[model_name] + std_factor * std_total)
-    - df_wbm[e_form_col]
+df_wbm[each_pred_col] = df_wbm[each_true_col] + (
+    (df_wbm[model_name] + std_factor * std_total) - df_wbm[e_form_col]
 )
 
-fig, metrics = hist_classified_stable_vs_hull_dist(
-    e_above_hull_true=df_wbm[e_above_hull_col],
-    e_above_hull_pred=e_above_hull_pred,
+fig = hist_classified_stable_vs_hull_dist(
+    df_wbm,
+    each_true_col=each_true_col,
+    each_pred_col=each_pred_col,
     which_energy=which_energy,
     # stability_threshold=-0.05,
     # rolling_acc=0,
-    # backend="matplotlib",
+    backend="plotly",
 )
-if hasattr(fig, "legend"):
-    legend_title = f"DAF = {metrics['DAF']:.3}"
-    fig.legend(loc="upper left", frameon=False, title=legend_title)
 
-fig.show()
+metrics = stable_metrics(df_wbm[each_true_col], df_wbm[each_pred_col])
+legend_title = f"DAF = {metrics['DAF']:.3}"
+
+if hasattr(fig, "legend"):  # matplotlib
+    fig.legend(loc="upper left", frameon=False, title=legend_title)
+else:  # plotly
+    fig.layout.legend.title.text = legend_title
+    fig.show()
 
 
 # %%
