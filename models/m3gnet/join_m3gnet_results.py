f5ace1ed31d602eeb8a5f0bc3654dcfb4ef5f902
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-02-02 15:09:41 -0800

    move m3gnet formation energy corrections to join_m3gnet_results.py

--- models/m3gnet/join_m3gnet_results.py
+++ models/m3gnet/join_m3gnet_results.py
@@ -2,18 +2,24 @@
 from __future__ import annotations
 
 import os
+import warnings
 from glob import glob
 
 import pandas as pd
-from pymatgen.analysis.phase_diagram import PDEntry
+from pymatgen.core import Structure
+from pymatgen.entries.compatibility import MaterialsProject2020Compatibility
+from pymatgen.entries.computed_entries import ComputedStructureEntry
+from pymatviz import density_scatter
 from tqdm import tqdm
 
-from matbench_discovery import today
+from matbench_discovery import ROOT, today
 from matbench_discovery.data import as_dict_handler
 from matbench_discovery.energy import get_e_form_per_atom
 
 __author__ = "Janosh Riebesell"
 __date__ = "2022-08-16"
+
+warnings.filterwarnings(action="ignore", category=UserWarning, module="pymatgen")
 
 
 # %%
@@ -44,16 +50,63 @@ df_m3gnet = pd.concat(dfs.values()).round(4)
 
 
 # %%
-df_m3gnet["e_form_per_atom_m3gnet"] = [
-    get_e_form_per_atom(PDEntry(row.m3gnet_structure.composition, row.m3gnet_energy))
-    for row in tqdm(df_m3gnet.itertuples(), total=len(df_m3gnet), disable=None)
+df_wbm = pd.read_json(
+    f"{ROOT}/data/wbm/2022-10-19-wbm-computed-structure-entries.json.bz2"
+).set_index("material_id")
+
+df_wbm["cse"] = [
+    ComputedStructureEntry.from_dict(x) for x in tqdm(df_wbm.computed_structure_entry)
 ]
-df_m3gnet.isna().sum()
+
+
+# %% transfer M3GNet energies and relaxed structures WBM CSEs
+# make sure we're not skipping m3gnet structures, other way around is fine
+# i.e. WBM CSEs for which M3GNet structures are missing
+assert not {*df_m3gnet.index} - {*df_wbm.index}
+
+cse: ComputedStructureEntry
+for cse in tqdm(df_wbm.cse):
+    if cse.entry_id in df_m3gnet.index:
+        # cse._energy is the uncorrected energy
+        cse._energy = df_m3gnet.loc[cse.entry_id, "m3gnet_energy"]
+        m3gnet_struct = Structure.from_dict(
+            df_m3gnet.loc[cse.entry_id, "m3gnet_structure"]
+        )
+        cse._structure = m3gnet_struct
+
+
+# %%
+df_wbm["e_form_per_atom_m3gnet_uncorrected"] = [
+    get_e_form_per_atom(cse) for cse in tqdm(df_wbm.cse)
+]
+
+
+# %% apply energy corrections
+out = MaterialsProject2020Compatibility().process_entries(
+    df_wbm.cse, verbose=True, clean=True
+)
+assert len(out) == len(df_wbm)
+
+
+# %% compute corrected formation energies
+df_wbm["e_form_per_atom_m3gnet"] = [
+    get_e_form_per_atom(cse) for cse in tqdm(df_wbm.cse)
+]
+
+e_form_m3gnet_cols = list(df_wbm.filter(like="m3gnet"))
+assert len(e_form_m3gnet_cols) == 2
+df_m3gnet[e_form_m3gnet_cols] = df_wbm[e_form_m3gnet_cols]
+
+
+# %%
+ax = density_scatter(
+    df=df_m3gnet, x="e_form_per_atom_m3gnet", y="e_form_per_atom_m3gnet_uncorrected"
+)
 
 
 # %%
 out_path = f"{module_dir}/{today}-m3gnet-wbm-{task_type}.json.gz"
-df_m3gnet = df_m3gnet.round(5)
+df_m3gnet = df_m3gnet.round(4)
 df_m3gnet.reset_index().to_json(out_path, default_handler=as_dict_handler)
 
 df_m3gnet.select_dtypes("number").to_csv(out_path.replace(".json.gz", ".csv"))
