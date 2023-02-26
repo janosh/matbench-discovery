ce6861e4385c088af760febf15d28d7b17c1efd1
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-02-25 18:51:39 -0800

    rm frontmatter.yml after consolidating with citation.cff
    pnpm i -D js-yaml
    fix link to landing page overlapping with nav
    add image to Wrenformer model card

--- site/src/routes/paper/+layout.server.ts
+++ site/src/routes/paper/+layout.server.ts
@@ -1,11 +1,13 @@
 import fs from 'fs'
+import yml from 'js-yaml'
 import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async ({ route }) => {
   const data = fs.readFileSync(`src/routes/${route.id}/+page.md`, `utf8`)
+  const cff = fs.readFileSync(`../citation.cff`, `utf8`)
 
   // Count the number of words using a regular expression
   const word_count = data.match(/\b\w+\b/g)?.length ?? null
 
-  return { word_count }
+  return { word_count, ...yml.load(cff) }
 }
