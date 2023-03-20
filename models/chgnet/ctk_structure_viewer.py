4f3e5be97839dee1230dad14a837de1d95cb113a
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-03-20 16:48:10 -0700

    add models/chgnet/ctk_struct_traj.py and models/wrenformer/analyze_wrenformer.py

--- models/chgnet/ctk_structure_viewer.py
+++ models/chgnet/ctk_structure_viewer.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import pandas as pd
 from crystal_toolkit.helpers.utils import hook_up_fig_with_struct_viewer
 
+from matbench_discovery.preds import PRED_FILES
+
 __author__ = "Janosh Riebesell"
 __date__ = "2023-03-07"
 
@@ -13,28 +15,24 @@ python scripts/ctk_structure_viewer.py
 Then open http://localhost:8000 in your browser.
 """
 
-df_plot = None
-min_e_diff = 0.1
 e_form_2000 = "e_form_per_atom_chgnet_2000"
 e_form_500 = "e_form_per_atom_chgnet_500"
 
-if df_plot is None:
-    from matbench_discovery.preds import PRED_FILES
+df_chgnet = pd.read_json(PRED_FILES.__dict__["CHGNet"].replace(".csv", ".json.gz"))
+df_chgnet = df_chgnet.set_index("material_id")
 
-    df_chgnet = pd.read_json(PRED_FILES.CHGNet.replace(".csv", ".json.gz"))
-    df_chgnet = df_chgnet.set_index("material_id")
+df_chgnet_2000 = pd.read_csv(PRED_FILES.__dict__["CHGNet"])
+df_chgnet_2000 = df_chgnet_2000.set_index("material_id").add_suffix("_2000")
+df_chgnet[list(df_chgnet_2000)] = df_chgnet_2000
 
-    df_chgnet_2000 = pd.read_csv(PRED_FILES.CHGNet)
-    df_chgnet_2000 = df_chgnet_2000.set_index("material_id").add_suffix("_2000")
-    df_chgnet[list(df_chgnet_2000)] = df_chgnet_2000
+df_chgnet_500 = pd.read_csv(PRED_FILES.__dict__["CHGNet"].replace("-06", "-04"))
+df_chgnet_500 = df_chgnet_500.set_index("material_id").add_suffix("_500")
+df_chgnet[list(df_chgnet_500)] = df_chgnet_500
 
-    df_chgnet_500 = pd.read_csv(PRED_FILES.CHGNet.replace("-06", "-04"))
-    df_chgnet_500 = df_chgnet_500.set_index("material_id").add_suffix("_500")
-    df_chgnet[list(df_chgnet_500)] = df_chgnet_500
-
-    e_form_abs_diff = "e_form_abs_diff"
-    df_chgnet[e_form_abs_diff] = abs(df_chgnet[e_form_2000] - df_chgnet[e_form_500])
-    df_plot = df_chgnet.round(3).query(f"{e_form_abs_diff} > {min_e_diff}")
+e_form_abs_diff = "e_form_abs_diff"
+min_e_diff = 0.1
+df_chgnet[e_form_abs_diff] = abs(df_chgnet[e_form_2000] - df_chgnet[e_form_500])
+df_plot = df_chgnet.round(3).query(f"{e_form_abs_diff} > {min_e_diff}")
 
 
 plot_labels = {
@@ -69,4 +67,4 @@ app = hook_up_fig_with_struct_viewer(
     # validate_id requires material_id to be hover_name
     validate_id=lambda id: id.startswith(("wbm-", "mp-", "mvc-")),
 )
-app.run_server(debug=True, port=8000)
+app.run(debug=True, port=8000)
---
+++ models/chgnet/ctk_trajectory_viewer.py
@@ -0,0 +1,218 @@
+# %%
+from __future__ import annotations
+
+import os
+import sys
+from typing import Callable
+
+import crystal_toolkit.components as ctc
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from chgnet.model import StructOptimizer as ChgnetRelaxer
+from chgnet.model.dynamics import TrajectoryObserver
+from crystal_toolkit.settings import SETTINGS
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+from m3gnet.models import Relaxer as M3gnetRelaxer
+from pymatgen.core import Lattice, Structure
+
+from matbench_discovery.data import df_wbm
+
+__author__ = "Janosh Riebesell"
+__date__ = "2023-03-22"
+
+module_dir = os.path.dirname(__file__)
+
+
+# %%
+init_struct_col = "initial_structure"
+df_cse = pd.read_json(f"{module_dir}/wbm-chgnet-bad-relax.json.gz").set_index(
+    "material_id",
+)
+
+
+# %%
+material_id = "wbm-1-7168"
+init_struct = Structure.from_dict(df_cse[init_struct_col].loc[material_id])
+
+final_struct = Structure.from_dict(
+    df_cse.computed_structure_entry.loc[material_id]["structure"],
+)
+
+init_spg = init_struct.get_space_group_info()
+final_spg = final_struct.get_space_group_info()
+print(f"{init_spg=}\n{final_spg=}")
+
+
+# %%
+chgnet_traj = ChgnetRelaxer().relax(init_struct)["trajectory"]
+m3gnet_traj = M3gnetRelaxer().relax(init_struct)["trajectory"]
+
+
+# %%
+e_col = "Energy (eV)"
+force_col = "Force (meV/Å)"
+vol_col = "Volume (Å<sup>3</sup)"
+df_chgnet, df_m3gnet = pd.DataFrame(), pd.DataFrame()
+
+for df, traj in ((df_chgnet, chgnet_traj), (df_m3gnet, m3gnet_traj)):
+    # convert array[float] to single float (only needed for m3gnet)
+    df[e_col] = list(map(float, traj.energies))
+    df[force_col] = [
+        1000
+        * np.linalg.norm(force, axis=1).mean()  # mean of norm of force on each atom
+        for force in traj.forces
+    ]
+    df[vol_col] = [Lattice(cell).volume for cell in traj.cells]
+    df.index.name = "Step"
+
+
+# %%
+row = df_wbm.loc[material_id]
+dft_energy = row.uncorrected_energy + row.e_correction_per_atom_mp2020 * row.n_sites
+print(f"{dft_energy=}")
+
+# don't rerun this cell (see duplicate id error below)
+struct_layouts: dict[str, html.Div] = {}
+struct_comps: dict[str, ctc.StructureMoleculeComponent] = {}
+
+
+# %%
+def plot_energy_and_forces(
+    df: pd.DataFrame,
+    step: int,
+    e_col: str,
+    force_col: str,
+    title: str,
+) -> go.Figure:
+    """Plot energy and forces as a function of relaxation step."""
+    fig = go.Figure()
+    # energy trace = primary y-axis
+    fig.add_trace(go.Scatter(x=df.index, y=df[e_col], mode="lines", name="Energy"))
+    # get energy line color
+    line_color = fig.data[0].line.color
+
+    # forces trace = secondary y-axis
+    fig.add_trace(
+        go.Scatter(
+            x=df.index,
+            y=df[force_col],
+            mode="lines",
+            name="Forces",
+            yaxis="y2",
+        ),
+    )
+
+    fig.update_layout(
+        template="plotly_white",
+        title=title,
+        xaxis={"title": "Relaxation Step"},
+        yaxis={"title": e_col},
+        yaxis2={"title": force_col, "overlaying": "y", "side": "right"},
+        legend=dict(yanchor="top", y=1, xanchor="right", x=1),
+    )
+
+    # vertical line at the specified step
+    fig.add_vline(x=step, line={"dash": "dash", "width": 1})
+
+    # horizontal line for DFT final energy
+    anno = {"text": "DFT final energy", "yanchor": "top"}
+    fig.add_hline(
+        y=dft_energy,
+        line=dict(dash="dot", width=1, color=line_color),
+        annotation=anno,
+    )
+
+    return fig
+
+
+app = Dash(prevent_initial_callbacks=True, assets_folder=SETTINGS.ASSETS_PATH)
+
+
+app_div = html.Div(
+    [
+        html.H1("Structure Relaxation Trajectory", style={"fontSize": "2em"}),
+        html.P("Drag slider to see structure at different relaxation steps."),
+    ],
+    style=dict(margin="auto", textAlign="center", maxWidth="1200px", padding="2em"),
+)
+
+for name, df, traj in (
+    ("CHGNet", df_chgnet, chgnet_traj),
+    ("M3GNet", df_m3gnet, m3gnet_traj),
+):
+    step_size = max(1, len(df) // 20)  # ensure slider has max 20 steps
+    slider = dcc.Slider(
+        id=f"{name}-slider", min=0, max=len(df) - 1, step=step_size, updatemode="drag"
+    )
+
+    # don't create layout twice, causes duplicate
+    # id error when restarting app in Jupyter notebook
+    if name not in struct_layouts:
+        struct_comps[name] = ctc.StructureMoleculeComponent(
+            id=f"{name}-structure",
+            struct_or_mol=init_struct,
+        )
+        struct_layouts[name] = struct_comps[name].layout()
+
+    spg = init_struct.get_space_group_info()
+    title = f"{material_id} - Spacegroup = {spg}"
+    graph = dcc.Graph(
+        id=f"{name}-fig",
+        figure=plot_energy_and_forces(df, 0, e_col, force_col, title),
+        style={"maxWidth": "50%"},
+    )
+
+    app_div.children += (
+        html.H2(name, style=dict(margin="2em 0 1em", fontSize="1.5em")),
+        slider,
+        html.Div(
+            [struct_layouts[name], graph],
+            style=dict(display="flex", gap="2em", placeContent="center"),
+        ),
+    )
+
+    def update_factory(
+        trajectory: TrajectoryObserver,
+        df_traj: pd.DataFrame,
+        material_id: str,
+        init_struct: Structure,
+        e_col: str,
+        force_col: str,
+    ) -> Callable[[int], tuple[Structure, go.Figure]]:
+        """Factory function to create update callback for each structure component."""
+
+        def update_structure(step: int) -> tuple[Structure, go.Figure]:
+            """Update the structure displayed in the StructureMoleculeComponent and the
+            dashed vertical line in the figure when the slider is moved.
+            """
+            lattice = trajectory.cells[step]
+            coords = trajectory.atom_positions[step]
+            init_struct.lattice = lattice
+            assert len(init_struct) == len(coords)
+            for site, coord in zip(init_struct, coords):
+                site.coords = coord
+
+            spg = init_struct.get_space_group_info()
+            title = f"{material_id} - Spacegroup = {spg}"
+
+            fig = plot_energy_and_forces(df_traj, step, e_col, force_col, title)
+            return init_struct, fig
+
+        return update_structure
+
+    app.callback(
+        Output(struct_comps[name].id(), "data"),
+        Output(graph, "figure"),
+        Input(slider, "value"),
+    )(update_factory(traj, df, material_id, init_struct, e_col, force_col))
+
+
+app.layout = app_div
+ctc.register_crystal_toolkit(app=app, layout=app.layout)
+
+# https://stackoverflow.com/a/74918941
+is_jupyter = "ipykernel" in sys.modules
+
+app.run(port=8000, use_reloader=not is_jupyter)
