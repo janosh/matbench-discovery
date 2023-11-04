3385b8578dccd6d21d98285d3752fcfdd6357854
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-11-03 19:09:53 -0700

    add CaptionedMetricsTable.svelte explaining n_materials vs n_structures diff

--- site/src/lib/index.ts
+++ site/src/lib/index.ts
@@ -1,3 +1,4 @@
+export { default as CaptionedMetricsTable } from './CaptionedMetricsTable.svelte'
 export { default as Footer } from './Footer.svelte'
 export { default as ModelCard } from './ModelCard.svelte'
 export { default as Nav } from './Nav.svelte'
