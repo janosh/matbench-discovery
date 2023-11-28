ec438ff12df0d980b2c962c9c5015f71f52daf3e
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-11-28 13:43:39 -0800

    refactor scripts/material_space.py, add plot axes and color bar labels

--- scripts/material_space.py
+++ scripts/material_space.py
@@ -1,58 +1,61 @@
-# %% Imports
+"""This script computes the UMAP projections of matminer features for the WBM and MP
+datasets. The resulting projections are saved to a .npz file.
+
+Needs pip install umap-learn.
+
+Uses Material Structure and Composition Featurizer
+"A critical examination of robustness and generalizability of machine learning
+prediction of materials properties"
+https://www.nature.com/articles/s41524-023-01012-9
+"""
+
+
+# %%
+from __future__ import annotations
+
 import os
-import re
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-# pip install umap-learn
 import umap
 from pymatgen.core import Structure
+from pymatviz.io import save_fig
 from tqdm import tqdm
 
+from matbench_discovery import DATA_DIR, PDF_FIGS, id_col
 from matbench_discovery.data import DATA_FILES
 
+__author__ = "Philipp Benner, Janosh Riebesell"
+__date__ = "2023-11-28"
 
-# %% Definitions
-basename = "material_space"
-data_dir = "../data"
 
-filename_features_mp = os.path.join(
-    data_dir, "mp/2023-02-07-mp-computed-structure-entries-features.csv.bz2"
+# %%
+mp_matminer_feat_path = f"{DATA_DIR}/mp/mp-final-structures-matminer-features.csv.bz2"
+wbm_matminer_feat_path = (
+    f"{DATA_DIR}/wbm/wbm-initial-structures-matminer-features.csv.bz2"
 )
-filename_features_wbm = os.path.join(
-    data_dir, "wbm/2022-10-19-wbm-computed-structure-entries-features.csv.bz2"
-)
-filename_umap = os.path.join(data_dir, f"{basename}.npz")
-filename_plot = f"{basename}.png"
-
-n_neighbors = 750
+n_umap_neighbors = 750
 
 
-# %% Material Structure and Composition Featurizer
-# "A critical examination of robustness and generalizability of machine learning prediction
-#  of materials properties"](https://www.nature.com/articles/s41524-023-01012-9) by
-# Kangming Li, Brian DeCost, Kamal Choudhary, Michael Greenwood, and Jason Hattrick-Simpers.
+# %%
+def featurize_dataframe(
+    df_in: pd.DataFrame | pd.Series,
+    struct_col: str = "structure",
+    ignore_errors: bool = True,
+    chunk_size: int = 30,
+) -> pd.DataFrame:
+    """Featurize a dataframe of structures using matminer Featurizers.
 
-
-def to_unitcell(structure):
-    [site.to_unit_cell(in_place=True) for site in structure.sites]
-    return structure
-
-
-def featurize_dataframe(df_in, col_id="structure", ignore_errors=True, chunksize=30):
-    """Featurize a dataframe using Matminter Structure featurizer
-
-    Parameters
-    ----------
-    df : Pandas.DataFrame
-        DataFrame with a column named "structure"
+    Args:
+        df_in (pd.DataFrame): DataFrame with a column named "structure"
+        struct_col (str, optional): Name of column containing structures.
+            Defaults to "structure".
+        ignore_errors (bool, optional): Whether to ignore errors. Defaults to True.
+        chunk_size (int, optional): Chunk size for parallelization. Defaults to 30.
 
     Returns:
-    -------
-    A DataFrame containing 273 features (columns)
-
+        pd.DataFrame: DataFrame with 273 columns containing matminer features
     """
     # For featurization
     from matminer.featurizers.base import MultipleFeaturizer
@@ -70,14 +73,13 @@ def featurize_dataframe(df_in, col_id="structure", ignore_errors=True, chunksize
         StructureComposition,
     )
 
-    if isinstance(df_in, pd.Series):
-        df = df_in.to_frame()
-    else:
-        df = df_in
-    df[col_id] = df[col_id].apply(to_unitcell)
+    df = df_in.to_frame() if isinstance(df_in, pd.Series) else df_in
+    for struct in df[struct_col]:
+        for site in struct:
+            site.to_unit_cell(in_place=True)
 
     # 128 structural feature
-    struc_feat = [
+    struct_feat = [
         SiteStatsFingerprint.from_preset("CoordinationNumber_ward-prb-2017"),
         SiteStatsFingerprint.from_preset("LocalPropertyDifference_ward-prb-2017"),
         StructuralHeterogeneity(),
@@ -85,145 +87,126 @@ def featurize_dataframe(df_in, col_id="structure", ignore_errors=True, chunksize
         ChemicalOrdering(),
     ]
     # 145 compositional features
-    compo_feat = [
+    comp_feat = [
         StructureComposition(Stoichiometry()),
         StructureComposition(ElementProperty.from_preset("magpie")),
         StructureComposition(ValenceOrbital(props=["frac"])),
         StructureComposition(IonProperty(fast=True)),
     ]
-    featurizer = MultipleFeaturizer(struc_feat + compo_feat)
-    # Set the chunksize used for Pool.map parallelisation
-    featurizer.set_chunksize(chunksize=chunksize)
-    featurizer.fit(df[col_id])
-    X = featurizer.featurize_dataframe(
-        df=df, col_id=col_id, ignore_errors=ignore_errors
+    featurizer = MultipleFeaturizer([*struct_feat, *comp_feat])
+    # Set the chunk size used for Pool.map parallelization
+    featurizer.set_chunksize(chunksize=chunk_size)
+    featurizer.fit(df[struct_col])
+    matminer_features = featurizer.featurize_dataframe(
+        df=df, col_id=struct_col, ignore_errors=ignore_errors
     )
     # check failed entries
     print("Featurization completed.")
-    failed = np.any(pd.isnull(X.iloc[:, df.shape[1] :]), axis=1)
-    if np.sum(failed) > 0:
-        print(f"Number failed: {np.sum(failed)}/{len(failed)}")
-    return X
+    failed = np.any(pd.isna(matminer_features.iloc[:, df.shape[1] :]), axis=1)
+    if sum(failed) > 0:
+        print(f"Number failed: {sum(failed)}/{len(failed)}")
+    return matminer_features
 
 
 # %%
 def featurize_file(
-    filename,
-    computed_structure=False,
-    input_col="initial_structure",
-    id_col="material_id",
-):
+    filename: str, struct_col: str = "initial_structure"
+) -> pd.DataFrame:
+    """Featurize pymatgen Structures in a file with matminer."""
     df_in = pd.read_json(filename).set_index(id_col)
 
-    if computed_structure:
-        df_in[input_col] = [
-            Structure.from_dict(x["structure"])
-            for x in tqdm(
-                df_in[input_col], leave=False, desc="Converting to PyMatgen Structure"
-            )
-        ]
-    else:
-        df_in[input_col] = [
-            Structure.from_dict(x)
-            for x in tqdm(
-                df_in[input_col], leave=False, desc="Converting to PyMatgen Structure"
-            )
-        ]
+    # ComputedStructureEntry dicts have a "structure" key, if that's missing it's a
+    # Structure dict
+    df_in[struct_col] = [
+        Structure.from_dict(x.get("structure", x))
+        for x in tqdm(df_in[struct_col], leave=False, desc="Hydrate structures")
+    ]
 
-    df_features = featurize_dataframe(df_in[input_col], col_id=input_col)
+    df_features = featurize_dataframe(df_in[struct_col], struct_col=struct_col)
 
-    return df_features.drop(input_col, axis=1)
+    return df_features.drop(columns=struct_col)
 
 
 # %%
-def select_features(df_features, threshold=0.95):
-    corr_matrix = df_features.corr(method="pearson", numeric_only=False).abs()
+def features_to_drop(df_in: pd.DataFrame, threshold: float = 0.95) -> list[str]:
+    """Get column names of features with correlation greater than threshold to drop."""
+    corr_matrix = df_in.corr(method="pearson", numeric_only=False).abs()
 
-    # Select upper triangle of correlation matrix
-    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+    # select upper triangle of correlation matrix
+    upper_tri = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
 
-    # Find features with correlation greater than our threshold
-    to_drop = [column for column in upper.columns if any(upper[column] > threshold)]
-
-    # Drop features
-    return df_features.drop(to_drop, axis=1)
-
-
-def import_features(filename, id_col="material_id"):
-    df = pd.read_csv(filename)
-    df = df.set_index(id_col)
-    return df
-
-
-# %%
-def plot_scatter(u, y, filename=None):
-    plt.scatter(u[:, 0], u[:, 1], c=y, cmap="Spectral", s=5)
-    plt.colorbar(boundaries=np.arange(min(y), max(y) + 2) - 0.5).set_ticks(
-        np.arange(min(y), max(y) + 1)
-    )
-    if filename is not None:
-        plt.savefig(filename)
-    plt.show()
+    # return features with correlation greater than the threshold
+    return [col for col in upper_tri if any(upper_tri[col] > threshold)]
 
 
 # %% Compute features and export to CSV
-if not os.path.exists(filename_features_mp):
-    print("Computing matminer mp features...")
+if not os.path.isfile(mp_matminer_feat_path):
+    print("Computing matminer features for MP...")
 
-    df_features = featurize_file(
-        DATA_FILES.mp_computed_structure_entries,
-        input_col="entry",
-        computed_structure=True,
+    df_mp_feats = featurize_file(
+        DATA_FILES.mp_computed_structure_entries, struct_col="entry"
     )
-    df_features.to_csv(filename_features_mp)
+    df_mp_feats.to_csv(mp_matminer_feat_path)
 
-if not os.path.exists(filename_features_wbm):
-    print("Computing matminer wbm features...")
+if not os.path.isfile(wbm_matminer_feat_path):
+    print("Computing matminer features for WBM...")
 
-    df_features = featurize_file(DATA_FILES.wbm_initial_structures)
-    df_features.to_csv(filename_features_wbm)
+    df_wbm_feats = featurize_file(DATA_FILES.wbm_initial_structures)
+    df_wbm_feats.to_csv(wbm_matminer_feat_path)
 
 
 # %%
-if not os.path.exists(filename_umap):
+umap_out_path = f"{DATA_DIR}/wbm/umap/umap-projections.csv.bz2"
+if not os.path.isfile(umap_out_path):
     print("Computing UMAP...")
 
-    df_in = import_features(filename_features_wbm)
-    df_mp = import_features(filename_features_mp)
-    df = pd.concat((df_in, df_mp))
+    df_mp = pd.read_csv(mp_matminer_feat_path).set_index(id_col)
+    df_wbm = pd.read_csv(wbm_matminer_feat_path).set_index(id_col)
 
     # Drop all rows containing NaN values
-    df_in = df_in.dropna(axis=0)
+    df_wbm = df_wbm.dropna(axis=0)
     df_mp = df_mp.dropna(axis=0)
 
-    # Combined data frame
-    df = pd.concat((df_in, df_mp))
-
     # Drop highly correlated features
-    df_mp = select_features(df_mp, threshold=0.95)
-    df_in = df_in[df_mp.columns]
-    df = df[df_mp.columns]
+    cols_to_drop = features_to_drop(df_mp, threshold=0.95)
+    df_mp = df_mp.drop(columns=cols_to_drop)
+    df_wbm = df_wbm.drop(columns=cols_to_drop)
 
-    # %% Create labels
-    y = [re.sub(r"^wbm-(\d+)-\d+$", r"\1", id) for id in df.index]
-    y = [re.sub(r"^(?:mp|mvc)-\d+$", r"0", id) for id in y]
-    y = pd.Series(y, index=df.index).astype(int).to_numpy()
+    # Combined data frame
+    df_all = pd.concat((df_wbm, df_mp))
+    df_all.index = df_all.index.str.replace(r"^(mp|mvc)-", "wbm-0")
+    df_all["wbm_step"] = df_all.index.str.split("-").str[1].astype(int)
 
-    # Train only on MP data
-    reducer = umap.UMAP(random_state=42, low_memory=False, n_neighbors=n_neighbors)
+    # train only on MP data
+    reducer = umap.UMAP(
+        n_neighbors=n_umap_neighbors, n_components=2, random_state=42, low_memory=False
+    )
     reducer.fit(df_mp)
 
-    # Transform everything
-    u = reducer.transform(df)
+    # transform everything
+    umap_points = reducer.transform(df_all)
+    cols = [f"UMAP {idx + 1}" for idx in range(umap_points.shape[1])]
+    df_umap = pd.DataFrame(umap_points, index=df_all.index, columns=cols)
 
-    np.savez(filename_umap, u=u, y=y)
+    df_umap.to_csv(umap_out_path)
+
+
+# %% Plot UMAP 2d projection
+df_umap = pd.read_csv(umap_out_path).set_index("wbm_step")
+
+assert (cols := list(df_umap)) == ["UMAP 1", "UMAP 2"]
+min_step, max_step = df_umap.index.min(), df_umap.index.max()
+ax = df_umap.plot.scatter(
+    *cols, c=df_umap.index, cmap="Spectral", s=5, figsize=(6, 4), colorbar=False
+)
+cbar = ax.figure.colorbar(
+    ax.collections[0],
+    boundaries=np.arange(min_step, max_step + 2) - 0.5,
+    ticks=range(min_step, max_step + 1),
+)
+cbar.ax.set_title("WBM step (0 = MP)", rotation=90, y=0.5, x=3, va="center")
 
 
 # %%
-if not os.path.exists(filename_plot):
-    print("Plotting material space...")
-
-    u = np.load(filename_umap)["u"]
-    y = np.load(filename_umap)["y"]
-
-    plot_scatter(u, y, filename=filename_plot)
+plt.tight_layout()
+save_fig(ax, f"{PDF_FIGS}/wbm-final-struct-matminer-features-2d-umap.png", dpi=300)
