5b274aaf9290b5b86015007a2eabf3abebf95d94
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-02-03 18:45:02 +0100

    add GNoME dataset URL and 'test script' for GNOME model (to fix tests)

---
+++ models/gnome/test_gnome.py
@@ -0,0 +1,16 @@
+# %%
+from pymatviz import density_scatter
+
+from matbench_discovery.enums import Key, Model
+from matbench_discovery.preds import df_preds
+
+__author__ = "Janosh Riebesell"
+__date__ = "2024-02-03"
+
+
+# %%
+df_preds[Model.gnome].hist(bins=100, figsize=(10, 10))
+
+
+# %%
+density_scatter(df=df_preds, x=Key.e_form, y=Model.gnome)
