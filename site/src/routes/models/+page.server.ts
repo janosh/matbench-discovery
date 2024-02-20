b4c98a8476cac46b4bf8350daf0640adb418770f
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-02-20 12:41:30 +0100

    rename model schema .yml + d.ts files

--- site/src/routes/models/+page.server.ts
+++ site/src/routes/models/+page.server.ts
@@ -1,6 +1,6 @@
 import type { ModelData } from '$lib'
 import model_stats from '$lib/model-stats-uniq-protos.json'
-import model_schema from '$root/tests/model-metadata-schema.yml'
+import model_schema from '$root/tests/model-schema.yml'
 import fs from 'fs'
 import { compile as json_to_ts } from 'json-schema-to-typescript'
 import { compile } from 'mdsvex'
@@ -37,10 +37,10 @@ export const load = async () => {
   return { models }
 }
 
-// keep model-metadata.d.ts in sync with model-metadata-schema.yml (source of truth)
+// keep model-schema.d.ts in sync with model-schema.yml (source of truth)
 // i.e. use json-schema-to-typescript to auto-convert YAML schema to TypeScript interface
 const model_md_type = await json_to_ts(model_schema, `ModelMetadata`, {
   style: { singleQuote: true, semi: false, printWidth: 100 },
 })
-const dts_out_file = `src/lib/model-metadata.d.ts`
+const dts_out_file = `src/lib/model-schema.d.ts`
 fs.writeFileSync(dts_out_file, model_md_type)
