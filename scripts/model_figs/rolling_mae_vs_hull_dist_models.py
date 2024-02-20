28f87fac93224d7b077d1744e6dc0e3db0f8ff0d
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-02-20 12:59:30 +0100

    fix ruff numpy RNG deprecation error

--- scripts/model_figs/rolling_mae_vs_hull_dist_models.py
+++ scripts/model_figs/rolling_mae_vs_hull_dist_models.py
@@ -60,17 +60,10 @@ else:
         if model in df_metrics.T.sort_values("MAE").index[show_n_best_models:]:
             trace.visible = "legendonly"  # show only top models by default
 
-    fig.layout.legend.update(
-        bgcolor="rgba(0,0,0,0)", title="", x=1.01, y=0, yanchor="bottom"
-    )
-    fig.layout.margin.update(l=5, r=5, t=5, b=55)
-
-    # plot marginal histogram of true hull distances along top of figure
-    # fixes plot artifacts by adding noise to avoid piling up data in some bins
-    # from rounded data
-    noise = np.random.random(len(df_preds)) * 1e-12
+    rng = np.random.default_rng()
+    small_noise = rng.random(len(df_preds)) * 1e-12
     counts, bins = np.histogram(
-        df_preds[Key.each_true] + noise, bins=100, range=fig.layout.xaxis.range
+        df_preds[Key.each_true] + small_noise, bins=100, range=fig.layout.xaxis.range
     )
     fig.add_scatter(
         x=bins, y=counts, name="Density", fill="tozeroy", showlegend=False, yaxis="y2"
