58c06b7268cb7de6ff795f07543da1dbdd4c7630
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-07-20 11:53:56 -0400

    models.py validate model-type on loading metadata yaml

--- matbench_discovery/models.py
+++ matbench_discovery/models.py
@@ -6,21 +6,31 @@ from typing import Any
 import yaml
 
 from matbench_discovery import ROOT
+from matbench_discovery.enums import ModelType
 
 # ignore underscore-prefixed directories for WIP model submissions
 MODEL_DIRS = glob(f"{ROOT}/models/[!_]*/")
 MODEL_METADATA: dict[str, dict[str, Any]] = {}
 
 for model_dir in MODEL_DIRS:
-    md_files = glob(f"{model_dir}*.yml")
-    if not 1 <= len(md_files) <= 2:
-        raise RuntimeError(f"expected 1 metadata file, got {md_files=} in {model_dir=}")
-    for md_file in md_files:
-        if md_file.endswith("aborted.yml"):
+    metadata_files = glob(f"{model_dir}*.yml")
+    if not 1 <= len(metadata_files) <= 2:
+        raise RuntimeError(
+            f"expected 1 metadata file, got {metadata_files=} in {model_dir=}"
+        )
+    for metadata_file in metadata_files:
+        if metadata_file.endswith("aborted.yml"):
             continue
         # make sure all required keys are non-empty
-        with open(md_file) as yml_file:
+        with open(metadata_file) as yml_file:
             model_data = yaml.full_load(yml_file)
 
         model_data["model_dir"] = model_dir
         MODEL_METADATA[model_data["model_name"]] = model_data
+
+        model_type = model_data.get("model_type")
+        try:
+            ModelType(model_type)  # check if model_type is valid
+        except ValueError as exc:
+            exc.add_note(f"{metadata_file=}")
+            raise
