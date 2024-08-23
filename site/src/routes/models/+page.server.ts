1496194cb93ee5de1b8ce6592f29cb0db83f82b7
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-08-23 11:31:21 +0200

    Test more plotting scripts in CI (#127)
    * rm .github/workflows/slow-tests.yml
    
    * only trigger test-scripts.yml on closed PRs and test more scripts
    
    * trigger test-scripts.yml
    
    * fix: can only give two args to bin df function, cbar_title_kwds -> kwargs (#128)
    
    * fix ModelCard missing notes due to casing issue
    
    * console.(error->trace) in site scripts
    
    * fix broken urls
    
    * fix script path
    
    ---------
    
    Co-authored-by: Rhys Goodall <rhys.goodall@outlook.com>

--- site/src/routes/models/+page.server.ts
+++ site/src/routes/models/+page.server.ts
@@ -7,7 +7,7 @@ import { compile } from 'mdsvex'
 import { dirname } from 'path'
 
 export const load = async () => {
-  const files = import.meta.glob(`$root/models/**/*.yml`, {
+  const files = import.meta.glob(`$root/models/[^_]**/[^_]*.yml`, {
     eager: true,
     import: `default`,
   }) as Record<string, ModelData>
@@ -18,7 +18,7 @@ export const load = async () => {
     .map(([key, metadata]) => {
       const { model_name } = metadata
       const stats = model_stats[model_name] as ModelData
-      if (!stats) console.error(`Missing stats for ${model_name}`)
+      if (!stats) console.trace(`Missing stats for ${model_name}`)
       return { ...metadata, ...(stats ?? {}), dirname: dirname(key) }
     }) as ModelData[]
 
@@ -28,7 +28,7 @@ export const load = async () => {
     for (const [key, note] of Object.entries(notes)) {
       const out = await compile(note)
       if (!out?.code) {
-        console.error(`Failed to compile model note ${model_name}/${key}`)
+        console.trace(`Failed to compile model note ${model_name}/${key}`)
         // remove outer p tags
       } else notes[key] = out.code.replace(/^\s<p>(.*)<\/p>\s$/, `$1`)
     }
