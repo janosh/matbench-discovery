3d7ce8257681d9b29117852f64e5add44f6923c5
JiaaoWANG-ut <wangjiaao0720@utexas.edu>
2025-03-04 23:46:27 -0600

    add alphanet to Matbench Discovery leaderboard

---
+++ models/alphanet/filt.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from matbench_discovery.data import df_wbm
+from matbench_discovery.enums import MbdKey
+
+e_form_7net_col = "e_form_per_atom_alphanet"
+
+csv_path = "./res_relax/alphanet.csv.gz"
+df_preds = pd.read_csv(csv_path).set_index("material_id")
+
+
+bad_mask = abs(df_preds[e_form_7net_col] - df_wbm[MbdKey.e_form_wbm]) > 5
+n_preds = len(df_preds[e_form_7net_col].dropna())
+print(f"{sum(bad_mask)=} is {sum(bad_mask) / len(df_wbm):.2%} of {n_preds:,}")
+df_preds.select_dtypes("number").to_csv("./res_relax/alphanet-no-bad.csv.gz")
---
+++ models/alphanet/test_alphanet.py
@@ -0,0 +1,91 @@
+## Borrowed from sevennet
+
+import os
+from collections.abc import Callable
+from typing import Any, Literal
+
+import numpy as np
+import pandas as pd
+import torch
+from ase import Atoms
+from ase.filters import ExpCellFilter, FrechetCellFilter
+from ase.optimize import FIRE, LBFGS
+from ase.optimize.optimize import Optimizer
+from pymatgen.io.ase import AseAtomsAdaptor
+from pymatviz.enums import Key
+from alphanet.infer.calc import AlphaNetCalculator
+from alphanet.models.model import AlphaNetWrapper
+from alphanet.config import All_Config
+from ase.io import write,read
+from tqdm import tqdm
+
+from matbench_discovery import WBM_DIR, timestamp
+from matbench_discovery.data import DataFiles, as_dict_handler, ase_atoms_from_zip
+from matbench_discovery.enums import Task
+
+
+# %% this config is editable
+smoke_test = False
+model_name = "alphanet"  
+config = All_Config().from_json("./mp.json")
+model = AlphaNetWrapper(config.model)
+model.load_state_dict( torch.load('./mp-0225-2.ckpt', map_location=torch.device('cuda')))
+task_type = Task.IS2RE
+job_name = f"{model_name}-wbm-{task_type}"
+ase_optimizer = "FIRE"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+ase_filter: Literal["frechet", "exp"] = "frechet"
+
+max_steps = 500
+force_max = 0.05  # Run until the forces are smaller than this in eV/A
+
+
+
+# %%
+id = 1 #we split initial structures into several parts
+
+os.makedirs(out_dir := "./res_relax", exist_ok=True)
+out_path = f"{out_dir}/{model_name}-{id:>03}.json.gz"
+
+data_path = f"./split_relax/part_{id}.extxyz"
+print(f"\nJob {job_name!r} running {timestamp}", flush=True)
+print(f"{data_path=}", flush=True)
+
+A_calc = AlphaNetCalculator(model=model, device = 'cuda')
+
+print(f"Read data from {data_path}")
+atoms_list = read(data_path,index=":", format = 'extxyz')
+relax_results: dict[str, dict[str, Any]] = {}
+
+filter_cls: Callable[[Atoms], Atoms] = {
+    "frechet": FrechetCellFilter,
+    "exp": ExpCellFilter,
+}[ase_filter]
+optim_cls: Optimizer = {"FIRE": FIRE, "LBFGS": LBFGS}[ase_optimizer]
+
+
+for atoms in tqdm(atoms_list, desc="Relaxing"):
+        mat_id = atoms.info[Key.mat_id]
+        if mat_id in relax_results:
+          continue
+    try:
+        atoms.calc = A_calc
+        if max_steps > 0:
+            atoms = filter_cls(atoms)
+            
+            optimizer = optim_cls(atoms, logfile="/dev/null")
+            optimizer.run(fmax=force_max, steps=max_steps)
+        energy = atoms.get_potential_energy()  # relaxed energy
+        # if max_steps > 0, atoms is wrapped by filter_cls, so extract with getattr
+        relaxed_struct = AseAtomsAdaptor.get_structure(getattr(atoms, "atoms", atoms))
+        relax_results[mat_id] = {"structure": relaxed_struct, "energy": energy}
+    except Exception as exc:
+        print(f"Failed to relax {mat_id}: {exec!r}")
+    
+
+df_out = pd.DataFrame(relax_results).T.add_prefix("alphanet_")
+df_out.index.name = Key.mat_id
+
+
+if not smoke_test:
+    df_out.reset_index().to_json(out_path, default_handler=as_dict_handler)
