e39cee57c6b0fbb9d442a9cc9adfffa031ace3db
yzchen08 <chenyuzhuo@zhejianglab.org>
2025-05-27 17:55:22 +0800

    add eqnorm-mptrj to Matbench Discovery leaderboard

---
+++ models/eqnorm/join_eqnorm_preds.py
@@ -0,0 +1,82 @@
+from glob import glob
+import warnings
+
+import pandas as pd
+from pymatgen.core import Structure
+from pymatgen.entries.compatibility import MaterialsProject2020Compatibility
+from pymatgen.entries.computed_entries import ComputedStructureEntry
+from pymatviz.enums import Key
+from tqdm import tqdm
+
+from matbench_discovery.data import as_dict_handler, df_wbm
+from matbench_discovery.energy import get_e_form_per_atom, mp_elemental_ref_energies
+from matbench_discovery.enums import DataFiles
+
+e_form_eqnorm_col = "e_form_per_atom_eqnorm"
+results = "./results"
+pot_name = "eqnorm"
+out_path = f"{results}/{pot_name}"
+files = sorted(glob(f"{results}/{pot_name}-*.json.gz"))
+
+dfs = {}
+for file_path in tqdm(files):
+    if file_path in dfs:
+        continue
+    df_i = pd.read_json(file_path, lines=True).set_index(Key.mat_id)
+    dfs[file_path] = df_i
+
+df_eqnorm = pd.concat(dfs.values()).round(4)
+
+if len(df_eqnorm) != len(df_wbm):  # make sure there is no missing structure
+    warnings.warn("Missing structures in eqnorm results")
+
+wbm_cse_path = DataFiles.wbm_computed_structure_entries.path
+df_wbm_cse = pd.read_json(wbm_cse_path, lines=True).set_index(Key.mat_id)
+
+df_wbm_cse[Key.computed_structure_entry] = [
+    ComputedStructureEntry.from_dict(dct)
+    for dct in tqdm(df_wbm_cse[Key.computed_structure_entry], desc="Hydrate CSEs")
+]
+
+# As SevenNet-0 (11July2024) is trained on 'uncorrected energy' of MPtrj,
+# MP formation energy corrections need to be applied
+
+
+# %% transfer energies and relaxed structures WBM CSEs since MP2020 energy
+# corrections applied below are structure-dependent (for oxides and sulfides)
+cse: ComputedStructureEntry
+for row in tqdm(df_eqnorm.itertuples(), total=len(df_eqnorm), desc="ML energies to CSEs"):
+    mat_id, struct_dict, energy, *_ = row
+    mlip_struct = Structure.from_dict(struct_dict)
+    cse = df_wbm_cse.loc[mat_id, Key.computed_structure_entry]
+    cse._energy = energy  # cse._energy is the uncorrected energy from MPtrj dataset (or vasp raw)  # noqa: E501, SLF001
+    cse._structure = mlip_struct  # noqa: SLF001
+    df_eqnorm.loc[mat_id, Key.computed_structure_entry] = cse
+
+
+orig_len = len(df_eqnorm)
+MaterialsProject2020Compatibility().process_entries(  # change in-place
+    df_eqnorm[Key.computed_structure_entry], verbose=True, clean=True
+)
+if len(df_eqnorm) != orig_len:
+    warnings.warn("Some structures were removed during energy correction")
+
+df_eqnorm[Key.formula] = df_wbm[Key.formula]
+
+df_eqnorm[e_form_eqnorm_col] = [
+    # see https://matbench-discovery.materialsproject.org/data#mp-elemental-reference-energies
+    # MP ref energies are the lowest energies found for unary structures of each element
+    get_e_form_per_atom(
+        dict(energy=cse.energy, composition=formula), mp_elemental_ref_energies
+    )
+    for formula, cse in tqdm(
+        df_eqnorm.set_index(Key.formula)[Key.computed_structure_entry].items(),
+        total=len(df_eqnorm),
+    )
+]
+df_eqnorm = df_eqnorm.round(4)
+
+df_eqnorm.select_dtypes("number").to_csv(f"{out_path}.csv.gz")  # save csv storable
+df_eqnorm.reset_index().to_json(
+    f"{out_path}.json.gz", default_handler=as_dict_handler, orient="records", lines=True
+)
---
+++ models/eqnorm/test_eqnorm_discovery.py
@@ -0,0 +1,98 @@
+"""Test eqnorm relaxation on the WBM dataset."""
+
+# %%
+import os
+from typing import Any
+
+import numpy as np
+import pandas as pd
+import torch
+from ase.filters import FrechetCellFilter
+from ase.optimize import FIRE, LBFGS
+from ase.optimize.optimize import Optimizer
+from pymatgen.io.ase import AseAtomsAdaptor
+from pymatviz.enums import Key
+from tqdm import tqdm
+
+from matbench_discovery import WBM_DIR, timestamp
+from matbench_discovery.data import as_dict_handler, ase_atoms_from_zip
+from matbench_discovery.enums import DataFiles, Task
+
+from eqnorm.calculator import EqnormCalculator
+
+
+def process_and_save(atoms_list, out_dir, job_id):
+    optim_cls: Optimizer = {"FIRE": FIRE, "LBFGS": LBFGS}[ase_optimizer]
+    out_path = f"{out_dir}/{model_name}-{job_id:>03}.json.gz"
+
+    relax_results: dict[str, dict[str, Any]] = {}
+    for atoms in tqdm(atoms_list, desc="Relaxing"):
+        mat_id = atoms.info[Key.mat_id]
+        if mat_id in relax_results:
+            continue
+        try:
+            atoms.calc = calc
+            if max_steps > 0:
+                atoms = FrechetCellFilter(atoms)
+                optimizer = optim_cls(atoms, logfile="/dev/null")
+                optimizer.run(fmax=force_max, steps=max_steps)
+            energy = atoms.get_potential_energy()  # relaxed energy
+            # if max_steps > 0, atoms is wrapped by FrechetCellFilter, so need to getattr
+            relaxed_struct = AseAtomsAdaptor.get_structure(getattr(atoms, "atoms", atoms))
+            relax_results[mat_id] = {"structure": relaxed_struct, "energy": energy}
+        except Exception as exc:
+            print(f"Failed to relax {mat_id}: {exc!r}")
+            continue
+
+    df_out = pd.DataFrame(relax_results).T.add_prefix("eqnorm_")
+    df_out.index.name = Key.mat_id
+
+    # %%
+    df_out.reset_index().to_json(
+        out_path, default_handler=as_dict_handler, orient="records", lines=True
+    )
+
+
+if __name__ == "__main__":
+    __author__ = "Yuzhuo Chen"
+    __date__ = "2025-03-28"
+
+    """History
+    2025-03-28: eqnorm_pro, first version
+    """
+    model_name = "eqnorm"
+    model_variant = "eqnorm-mptrj"
+    train_progress = "0.9"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cpu")
+    ase_optimizer = "FIRE"
+    out_dir = "./results"
+    os.makedirs(out_dir, exist_ok=True)
+
+    max_steps = 500
+    force_max = 0.02
+
+    slurm_array_task_count = int(os.getenv("TOTAL_TASKS", "1"))
+    slurm_array_task_id = int(os.getenv("SLURM_ARRAY_TASK_ID", "0"))
+
+    task_type = Task.IS2RE
+    job_name = f"{model_name}-wbm-{task_type}"
+    data_path = {Task.IS2RE: DataFiles.wbm_initial_atoms.path}[task_type]
+    print(f"\nJob {job_name!r} running {timestamp}", flush=True)
+    print(f"{data_path=}", flush=True)
+
+    print(f"Read data from {data_path}")
+    zip_filename = f"{WBM_DIR}/2024-08-04-wbm-initial-atoms.extxyz.zip"
+    atoms_list = ase_atoms_from_zip(zip_filename)
+
+    atoms_list = atoms_list[slurm_array_task_id::slurm_array_task_count]
+
+    # load my own calculator
+    calc = EqnormCalculator(
+        model_name=model_name,
+        model_variant=model_variant,
+        train_progress=train_progress,
+        device=device
+        )
+
+    process_and_save(atoms_list, out_dir, slurm_array_task_id)
