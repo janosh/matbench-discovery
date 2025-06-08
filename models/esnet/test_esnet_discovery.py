c59b357b32e902386173c1ea7509c81872f7e254
Ling Shi <695974893@qq.com>
2025-06-09 00:13:21 +0800

    Update test_esnet_discovery.py

--- models/esnet/test_esnet_discovery.py
+++ models/esnet/test_esnet_discovery.py
@@ -1,35 +1,38 @@
 import os
-import pickle as pk
-from collections import Counter
-from typing import Any
+from typing import List, Dict, Any
 
-import numpy as np
 import pandas as pd
 import torch
 import wandb
-from esnet.graphs import PygGraph, PygStructureDataset
-from esnet.load_triples import Triples
-from esnet.models.comformer import iComformer
-from ignite.handlers import Checkpoint
 from pymatgen.core import Structure
 from pymatgen.io.jarvis import JarvisAtomsAdaptor
 from pymatviz.enums import Key
-from sklearn.metrics import r2_score
-from torch_geometric.data import Data
+from sklearn.metrics import r2_score,accuracy_score, f1_score, precision_score
 from tqdm import tqdm
-
 from matbench_discovery import today
 from matbench_discovery.data import df_wbm
 from matbench_discovery.enums import DataFiles, MbdKey, Model, Task
 from matbench_discovery.hpc import slurm_submit
 from matbench_discovery.plots import wandb_scatter
 
+from esnet.models.comformer import iComformer
+from esnet.graphs import PygGraph, PygStructureDataset, prepare_pyg_line_graph_batch, PygKnowledgeAndStructureDataset
+from esnet.models.comformer import iComformerConfig
+from esnet.data import load_pyg_graphs
+from ignite.handlers import Checkpoint
+import pickle as pk
+from esnet.load_triples import Triples
+from collections import Counter
+import numpy as np
+from torch_geometric.data import Data
+from torch.utils.data import DataLoader
+import json
+
 __author__ = "sl"
-__date__ = "2025-05-13"
+__date__ = "2025-06-08"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 adaptor = JarvisAtomsAdaptor()
-
 
 def atoms_to_graph(atoms: Structure) -> PygGraph:
     """Convert structure to Atom."""
@@ -47,63 +50,6 @@ def atoms_to_graph(atoms: Structure) -> PygGraph:
     )
 
 
-def graph_build(atoms: Structure) -> Data:
-    g = atoms_to_graph(atoms)
-    features = PygStructureDataset.get_attribute_lookup("cgcnn")
-    z = g.x
-    g.atomic_number = z
-    z = z.type(torch.IntTensor).squeeze()
-    f = torch.tensor(features[z]).type(torch.FloatTensor)
-    if g.x.size(0) == 1:
-        f = f.unsqueeze(0)
-    g.x = f
-    g.batch = torch.zeros(1, dtype=torch.int64)
-    return g
-
-
-with open("/home/sl/project/ESNet/graphs/RotatE_128_64.pkl", "rb") as f:
-    rotate_emb = pk.load(f)  # noqa: S301
-
-
-def load_elements_knowledge(elements: list[str], rotate_emb: dict[str, Any]) -> Data:
-    data = Triples()
-
-    entity = rotate_emb["entity"]
-    element_counts = Counter(elements)
-    total_elements = len(elements)
-    element_ratios = {
-        element: count / total_elements for element, count in element_counts.items()
-    }
-    template = entity[0]
-    features = np.zeros((len(element_counts), len(np.array(template))))
-
-    attributes = []
-
-    for z, (element, ratios) in enumerate(element_ratios.items()):
-        if element in data.entities:
-            attribute_id = [h for (r, h) in data.t2rh[data.entity2id[element]]]
-            attributes.extend(attribute_id)
-
-        key = data.entity2id[element]
-        x = entity[int(key)]
-        if x is not None:
-            features[z, :] = x * ratios
-
-    attributes = sorted(set(attributes))
-    attr_features = np.zeros((len(attributes), len(np.array(template))))
-
-    for t, key in enumerate(attributes):
-        attr_features[t, :] = entity[int(key)]
-
-    f = torch.tensor(features).type(torch.get_default_dtype())
-    attr_f = torch.tensor(attr_features).type(torch.get_default_dtype())
-    node_emb = torch.cat((f, attr_f), 0)
-    knowledge_features = Data(x=node_emb)
-    knowledge_features.batch = torch.zeros(1, dtype=torch.int64)
-
-    return knowledge_features
-
-
 model_name = Model.esnet
 task_type = Task.IS2RE
 target_col = MbdKey.e_form_dft
@@ -111,9 +57,13 @@ job_name = f"{model_name}/{today}-wbm-{task_type}"
 module_dir = os.path.dirname(__file__)
 out_dir = os.getenv("SBATCH_OUTPUT", f"{module_dir}/{job_name}")
 
-model = iComformer()
+config_file = "models/esnet/config.json"
+config = json.load(open(config_file,"r"))
+esnet_config = iComformerConfig(**config['model'])
+
+model = iComformer(esnet_config)
 e_form_checkpoint = torch.load(
-    "/home/sl/project/ESNet/checkpoint/checkpoint_eform_500.pt", map_location="cuda:0"
+    "/home/sl/project/ESNet/checkpoint/checkpoint_esnet_259.pt", map_location="cuda:0"
 )
 to_load = {
     "model": model,
@@ -152,6 +102,39 @@ df_in[input_col] = [
     Structure.from_dict(dct) for dct in tqdm(df_in[input_col], disable=None)
 ]
 
+atoms = []
+for struc in df_in[input_col]:
+
+    lattice = struc.lattice.matrix
+    coords = struc.frac_coords
+    elements = [site.specie.symbol for site in struc]
+    abc = [struc.lattice.a, struc.lattice.b, struc.lattice.c]
+    angles = [struc.lattice.alpha, struc.lattice.beta, struc.lattice.gamma]
+
+    atom = {
+        "lattice_mat": lattice,
+        "coords": coords,
+        "elements": elements,
+        "abc": abc,
+        "angles" : angles,
+        "cartesian": False,
+        "props": [""] * len(elements)
+    }
+
+    atoms.append(atom)
+
+df_wbm["atoms"] = atoms
+
+with open("/home/sl/project/ESNet/graphs/RotatE_128_64.pkl", "rb") as f:
+    rotate_emb = pk.load(f)  # noqa: S301
+
+graphs = []
+for struc in df_in[input_col]:
+    g = atoms_to_graph(struc)
+    graphs.append(g)
+
+graphs = [x.cpu() for x in graphs]
+
 
 # %%
 run_params = {
@@ -171,31 +154,42 @@ wandb.init(project="matbench-discovery", name=job_name, config=run_params)
 
 
 # %% Predict
-# eform 均值与方差
-std_eform = 1.073214
-mean_eform = -1.652392
+data = PygKnowledgeAndStructureDataset(
+        df_wbm,
+        graphs,
+        ele2emb,
+        target="e_form_per_atom_mp2020_corrected",
+        atom_features="atomic_number",
+        line_graph=True,
+        id_tag="material_id",
+        classification=False,
+        neighbor_strategy="k-nearest",
+    )
+
+        
+wbm_loader = DataLoader(
+        data,
+        batch_size=64,
+        shuffle=False,
+        collate_fn=PygKnowledgeAndStructureDataset.collate_line_graph,
+        drop_last=False,
+        num_workers=1,
+)
+
+e_form_values = []
 
 model.eval()
 e_form_preds: dict[str, float] = {}
-with torch.no_grad():  # get predictions
-    for material_id, atoms in tqdm(
-        df_in[input_col].items(),
-        total=len(df_in),
-        desc=f"Predicting {target_col=} {task_type}",
-    ):
-        elements = [site.specie.symbol for site in atoms.sites]
-        g = graph_build(atoms)
-        kge = load_elements_knowledge(elements, rotate_emb)
-        out_data = model([g.to(device), g.to(device), kge.to(device), g.to(device)])
-        out_data = out_data.cpu().numpy()
-        e_form = out_data * std_eform + mean_eform
-        e_form_preds[material_id] = e_form
+with torch.no_grad():
+    for data in wbm_loader:
+        x, y = prepare_pyg_line_graph_batch(data, device=device, non_blocking=False)
+        output = model(x)
+        e_form = output.cpu().numpy().tolist()
+        e_form_values.extend(e_form)
 
 pred_col = "e_form_per_atom_esnet"
-df_wbm[pred_col] = e_form_preds
+df_wbm[pred_col] = e_form_values
 
-df_wbm[pred_col] -= df_wbm.e_correction_per_atom_mp_legacy
-df_wbm[pred_col] += df_wbm.e_correction_per_atom_mp2020
 df_wbm[pred_col].round(4).to_csv(f"{module_dir}/{today}-{model_name}-wbm-IS2RE.csv.gz")
 
 
@@ -210,6 +204,5 @@ print("R2:", R2)
 
 title = f"{model_name} {task_type} {MAE=:.4} {R2=:.4}"
 print("title:", title)
-
 
 wandb_scatter(table, fields=dict(x=target_col, y=pred_col), title=title)
