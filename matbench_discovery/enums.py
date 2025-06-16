51a3b0553c5569ddde18f5963cab654b747a0e8c
Janosh Riebesell <janosh.riebesell@gmail.com>
2025-06-16 15:10:00 -0400

    fix test_df_each_(pred|err)

--- matbench_discovery/enums.py
+++ matbench_discovery/enums.py
@@ -277,7 +277,6 @@ class Model(Files, base_dir=f"{ROOT}/models"):
     alchembert = auto(), "alchembert/alchembert.yml"
 
     # AlphaNet: https://arxiv.org/abs/2501.07155
-    alphanet_mptrj = auto(), "alphanet/alphanet-mptrj.yml"
     alphanet_oma = auto(), "alphanet/alphanet-oma.yml"
     # alignn with global pooling: https://arxiv.org/abs/2106.01829
     alignn = auto(), "alignn/alignn.yml"
