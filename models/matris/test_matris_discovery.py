ec5affc2fde8afff46005ab745cd8fe4df3f7e95
Yuanchang Zhou <67250796+Asecretboy@users.noreply.github.com>
2026-01-09 20:03:23 +0800

    Remove print statement from test_matris_discovery.py

--- models/matris/test_matris_discovery.py
+++ models/matris/test_matris_discovery.py
@@ -94,7 +94,7 @@ run_params = {
 
 run_name = f"{job_name}-{slurm_array_task_id}"
 
-print(df_in)
+
 # %%
 relax_results: dict[str, dict[str, Any]] = {}
 input_col = {Task.IS2RE: Key.initial_struct, Task.RS2RE: Key.final_struct}[task_type]
