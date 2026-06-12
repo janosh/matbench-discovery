404a6016fbfe761b41f7781911a688ef78f2b148
Hyuntae Cho <robert.cho@samsung.com>
2026-06-12 16:24:30 +0900

    add comments in install.sh

--- models/equflash/install.sh
+++ models/equflash/install.sh
@@ -3,4 +3,6 @@ uv venv --python 3.12
 source .venv/bin/activate
 uv pip install torch==2.9.1+cu126 --extra-index-url https://download.pytorch.org/whl/cu126
 uv pip install -r requirements.txt
+
+# Intentional workaround: only the exercised fairchem code path is used.
 uv pip install fairchem-core==1.10.0 scipy==1.16.1 --no-deps
