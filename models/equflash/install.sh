3eb8824ef8f03188283b25087922d366cfe1d661
Hyuntae Cho <robert.cho@samsung.com>
2026-06-12 16:22:38 +0900

    delete unsafe flags in install.sh

--- models/equflash/install.sh
+++ models/equflash/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 uv venv --python 3.12
 source .venv/bin/activate
-uv pip install -r requirements.txt --index-strategy unsafe-best-match
+uv pip install torch==2.9.1+cu126 --extra-index-url https://download.pytorch.org/whl/cu126
+uv pip install -r requirements.txt
 uv pip install fairchem-core==1.10.0 scipy==1.16.1 --no-deps
